5e316ff6effcb24e99df3ab37e4bf35d73f0161a
Henryikenna <ikennaunegbu10@gmail.com>
2023-03-14 03:34:42 +0100

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/views/login_view.dart';
+import 'package:mynotes/views/register_view.dart';
+import 'package:mynotes/views/verify_email_view.dart';
 
 import 'firebase_options.dart';
 
@@ -13,6 +16,10 @@ void main() {
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
+      routes: {
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView(), 
+      },
     ),
   );
 }
@@ -22,11 +29,7 @@ class HomePage extends StatelessWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-  return Scaffold( 
-      appBar: AppBar(
-        title: const Text('Home'),
-        ),
-        body: FutureBuilder(
+  return FutureBuilder(
           future: Firebase.initializeApp(
             options: DefaultFirebaseOptions.currentPlatform,
           ),
@@ -34,17 +37,22 @@ class HomePage extends StatelessWidget {
             switch (snapshot.connectionState) {
               case ConnectionState.done:
               final user = FirebaseAuth.instance.currentUser;
-              if (user?.emailVerified ?? false){
-                print('You are a verified user');
-              } else {
-                print('You need to verify your email first.');
+              if (user != null){
+                if (user.emailVerified) {
+                  print('Email is verified');
+                } else{
+                  return const VerifyEmailView();
+                }
+              } else{
+                return const LoginView();
               }
                 return const Text('Done');
-          default:
-          return const Text('Loading...'); 
+              default:
+          return const CircularProgressIndicator();
             }
           }, 
-        ),
-    );
+        );
   } 
 }
+
+
